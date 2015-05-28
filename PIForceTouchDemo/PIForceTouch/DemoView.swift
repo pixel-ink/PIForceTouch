3ae36f811a21f749cb2154652aacc04f6bb16ad7
pixel-ink <fujiwara@yoshiki.mobi>
2015-05-28 10:42:17 +0900

    DemoView

---
+++ PIForceTouchDemo/PIForceTouch/DemoView.swift
@@ -0,0 +1,6 @@
+
+import UIKit
+
+class DemoView : UIView {
+//TODO
+}
--- PIForceTouchDemo/PIForceTouch/ViewController.swift
+++ PIForceTouchDemo/PIForceTouch/ViewController.swift
@@ -3,9 +3,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var demoView: DemoView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    demoView.layer.borderColor = UIColor.whiteColor().CGColor
+    demoView.layer.borderWidth = 1.0
   }
 
   override func didReceiveMemoryWarning() {
